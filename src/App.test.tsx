7933f178fbd475155b614ed387475a030fef5acf
Laura <lauraawarr@gmail.com>
2024-03-21 11:10:53 -0700

    Update togglebot logo (#1)

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders demo app', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/demo application/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
